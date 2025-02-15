173a78047508f20976fe04770d391317670f0af5
cris476 <152157932+JohnStiven20@users.noreply.github.com>
2025-02-15 09:15:06 +0100

    renombre de componente

---
+++ src/components/Task.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-card class="pa-5 mb-4">
+        <v-card-title class="text-h5 text-center">{{ title }}</v-card-title>
+        <v-card-text>
+            {{ description }}
+        </v-card-text>
+        <v-row>
+            <v-col cols="6">
+                <v-btn type="submit" color="red" block>
+                    Eliminar
+                </v-btn>
+            </v-col>
+            <v-col cols="6">
+                <v-btn type="submit" color="blue" block>
+                    Editar
+                </v-btn>
+            </v-col>
+        </v-row>
+    </v-card>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            default: "Sin descripción"
+        }
+    }
+};
+</script>
