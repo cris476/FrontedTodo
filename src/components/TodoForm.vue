c39fe31e38bcdd17107b1218ed0dcda39f82c67d
cris476 <152157932+JohnStiven20@users.noreply.github.com>
2025-02-15 09:08:33 +0100

    Formulario del ToDO

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-card class="pa-5 mb-4">
+        <v-card-title class="text-h5 text-center">Añadir Tarea</v-card-title>
+        <v-form @submit.prevent="submitTask">
+            <v-text-field v-model="title" label="Título" outlined required></v-text-field>
+            <v-textarea v-model="description" label="Descripción" outlined required></v-textarea>
+            <v-btn type="submit" color="primary" block class="mb-3">Añadir Tarea</v-btn>
+        </v-form>
+    </v-card>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: "",
+            description: ""
+        };
+    },
+    methods: {
+        submitTask() {
+            if (this.title.trim() && this.description.trim()) {
+                this.$emit("newNota", {
+                    title: this.title,
+                    description: this.description
+                });
+
+                this.title = "";
+                this.description = "";
+            }
+        }
+    }
+};
+</script>
+
