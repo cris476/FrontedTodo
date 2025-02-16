cfa1c300bb5eb634f122b45609da75bca0731cfd
cris476 <152157932+JohnStiven20@users.noreply.github.com>
2025-02-16 11:09:36 +0100

    archvio de configuracion de axios

---
+++ src/Service/axiosConfig.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const API_URL = "http://127.0.0.1:9000/api";
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      console.log("toke localStorage");
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
