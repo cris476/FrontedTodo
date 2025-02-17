7b056be62e10dc797122b0ce172e3ac8f95af542
cris476 <152157932+JohnStiven20@users.noreply.github.com>
2025-02-17 22:37:18 +0100

    editar las notas

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,34 +1,90 @@
 <template>
     <v-card class="pa-5 mb-4">
-        <v-card-title class="text-h5 text-center">{{ title }}</v-card-title>
-        <v-card-text>
-            {{ description }}
-        </v-card-text>
-        <v-row>
-            <v-col cols="6">
-                <v-btn type="submit" color="red" block>
-                    Eliminar
-                </v-btn>
-            </v-col>
-            <v-col cols="6">
-                <v-btn type="submit" color="blue" block>
-                    Editar
-                </v-btn>
-            </v-col>
-        </v-row>
+        <v-container v-if="edit">
+            <v-card-title class="text-h5 text-center">{{ localTitle }}</v-card-title>
+            <v-card-text>
+                {{ localDescription }}
+            </v-card-text>
+            <v-row>
+                <v-col cols="6">
+                    <v-btn type="button" color="red" block @click="deleteTask">
+                        Eliminar
+                    </v-btn>
+                </v-col>
+                <v-col cols="6">
+                    <v-btn type="button" color="blue" block @click="editTask">
+                        Editar
+                    </v-btn>
+                </v-col>
+            </v-row>
+        </v-container>
+
+        <v-container v-else>
+            <div>{{ id }}</div>
+            <v-text-field v-model="localTitle" outlined></v-text-field>
+            <v-textarea v-model="localDescription" outlined></v-textarea>
+            <v-row>
+                <v-col cols="6">
+                    <v-btn type="button" color="green" block @click="confirmEdit">
+                        Confirmar
+                    </v-btn>
+                </v-col>
+                <v-col cols="6">
+                    <v-btn type="button" color="red" block @click="cancelEdit">
+                        Cancelar
+                    </v-btn>
+                </v-col>
+            </v-row>
+        </v-container>
     </v-card>
 </template>
 
 <script>
+
+
 export default {
     props: {
+        id: {
+            type: Number,
+            required: true
+        },
         title: {
             type: String,
             required: true
         },
         description: {
             type: String,
-            default: "Sin descripción"
+            required: true
+        }
+    },
+    data() {
+        return {
+            edit: true,
+            localTitle: this.title,
+            localDescription: this.description
+        };
+    },
+    methods: {
+        editTask() {
+            this.edit = false;
+        },
+        confirmEdit() {
+            this.$emit("updateTask", {
+                id: this.id,
+                title: this.localTitle,
+                description: this.localDescription
+            });
+            this.edit = true;
+        },
+        cancelEdit() {
+            this.localTitle = this.title;
+            this.localDescription = this.description;
+            this.edit = true;
+        },
+        deleteTask() {
+            this.$emit("deleteTask", {
+                id: this.id
+            });
         }
     }
 };
