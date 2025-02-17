286dfeebe1b5bf2669b27fed410f7bc9c9be17b0
cris476 <152157932+JohnStiven20@users.noreply.github.com>
2025-02-17 22:39:31 +0100

    refactorizacion del login view

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,10 @@
 <template>
     <v-container class="">
         <v-card class="pa-5 v-card mx-auto mt-15" max-width="350">
-            <LoginForm></LoginForm>
+            <LoginForm @login=loginView>
+
+
+            </LoginForm>
         </v-card>
     </v-container>
 </template>
@@ -9,43 +12,34 @@
 <style scoped>
 .v-card {
     border-radius: 10px;
-
 }
 </style>
 
 <script>
 import LoginForm from '../components/LoginForm.vue';
-
-
-
+import { login } from '../Service/authService';
 
 export default {
     components: {
         LoginForm
     },
-    data() {
-        return {
-            isValid: false,
-            user: {
-                email: '',
-                password: '',
-            },
-            rules: {
-                required: (value) => !!value || 'Este campo es obligatorio',
-                email: (value) => {
-                    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-                    return pattern.test(value) || 'Correo electrónico inválido'
-                },
-                min: (min) => (value) =>
-                    (value && value.length >= min) ||
-                    `Debe tener al menos ${min} caracteres`,
-            },
-        }
-    },
     methods: {
-        login() {
-            
+        async loginView(loginData) {
+
+            try {
+                console.log(loginData)
+                const response = await login(loginData);
+                console.log(response)
+
+                if (response && response.data.validation) {
+                    localStorage.setItem("token", response.data.access_token)
+                    this.$router.push('/todoView');
+                }
+            } catch (error) {
+                console.log(error)
+            }
         },
+
     },
 }
 </script>
