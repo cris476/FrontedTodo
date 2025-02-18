45ddb4fe7854d8fc4dbddb32dce6c0ce2eb4b3ef
cris476 <152157932+JohnStiven20@users.noreply.github.com>
2025-02-18 16:30:32 +0100

    refactorizacion

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,11 +1,12 @@
 <template>
     <v-container class="">
         <v-card class="pa-5 v-card mx-auto mt-15" max-width="350">
-            <LoginForm @login=loginView>
-
-
+            <LoginForm @login=loginView :isloading="isloading">
             </LoginForm>
         </v-card>
+        <v-snackbar v-model="showError" color="red" top timeout="3000">
+            {{ errorMessage }}
+        </v-snackbar>
     </v-container>
 </template>
 
@@ -20,25 +21,42 @@ import LoginForm from '../components/LoginForm.vue';
 import { login } from '../Service/authService';
 
 export default {
+
+    name: 'LoginView',
     components: {
         LoginForm
+    },
+
+    data() {
+        return {
+            isloading: false,
+            showError: false,
+            errorMessage: "",
+        }
     },
     methods: {
         async loginView(loginData) {
 
             try {
-                console.log(loginData)
+                this.isloading = true;
                 const response = await login(loginData);
-                console.log(response)
-
                 if (response && response.data.validation) {
                     localStorage.setItem("token", response.data.access_token)
                     this.$router.push('/todoView');
                 }
             } catch (error) {
-                console.log(error)
+                if (error.response) {
+                    this.isloading = false;
+                    this.showErrorMessage(error.response.data.errors.message[0]);
+                } else {
+                    this.showErrorMessage("Error en el servidor. Inténtalo más tarde.");
+                }
             }
         },
+        showErrorMessage(message) {
+            this.errorMessage = message;
+            this.showError = true;
+        }
 
     },
 }
